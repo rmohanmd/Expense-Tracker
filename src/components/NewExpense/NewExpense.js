d884e1d7ecb0213f28585362203523ccdbd24bf2
rmohanmd <rmohan.md@gmail.com>
2022-10-05 15:38:28 -0400

    Add callback functions to move form data to expenses array

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const onFormSubmit = (submitedExpenseData) => {
+    const expenseData = {
+      ...submitedExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onFormSubmit={onFormSubmit} />
     </div>
   );
 };
