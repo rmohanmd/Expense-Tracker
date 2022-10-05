1dd2841822e693044019ec8e6e1d110ede31974a
rmohanmd <rmohan.md@gmail.com>
2022-10-05 11:51:12 -0400

    Changed from hardcoded data to varibles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 19 2019</div>
+      <div>{expenseDate.toISOString()}</div>
       <div claaName="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{expenseTitle}</h2>
       </div>
-      <div className="expense-item__price">$249.67</div>
+      <div className="expense-item__price">${expenseAmount}</div>
     </div>
   );
 }
