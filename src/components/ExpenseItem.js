7090d843f8713f4007a1a8a6154edcb2e2d210b0
rmohanmd <rmohan.md@gmail.com>
2022-10-05 12:11:42 -0400

    Added expense object and passed as props to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem({ title, amount, date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div claaName="expense-item__description">
-        <h2>{expenseTitle}</h2>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+      </div>
+      <div className="expense-item__price">${amount}</div>
     </div>
   );
 }
